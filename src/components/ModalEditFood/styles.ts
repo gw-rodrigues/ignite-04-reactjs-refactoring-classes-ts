0d9bddde3f07570c69e57ce363a07772752bab4c
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-04-18 22:18:09 +0200

    Add form interface

--- src/components/ModalEditFood/styles.ts
+++ src/components/ModalEditFood/styles.ts
@@ -1,7 +1,11 @@
 import styled from 'styled-components';
 import { Form as Unform } from '@unform/web';
 
-export const Form = styled(Unform)`
+interface FormProps {
+  ref?: any
+}
+
+export const Form = styled(Unform)<FormProps>`
   padding: 48px 40px;
   display: flex;
   flex-direction: column;
