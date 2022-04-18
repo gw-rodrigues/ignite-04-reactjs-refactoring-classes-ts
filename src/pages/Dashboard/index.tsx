5c99e73f2b139fe9caabe6561ee250a5c29630a7
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-04-18 11:12:01 +0200

    Importing types

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -2,19 +2,12 @@ import { useEffect, useState } from 'react';
 
 import Header from '../../components/Header';
 import api from '../../services/api';
-import Food from '../../components/Food';
+import { Food } from '../../components/Food';
 import ModalAddFood from '../../components/ModalAddFood';
 import ModalEditFood from '../../components/ModalEditFood';
 import { FoodsContainer } from './styles';
 
-interface FoodProps {
-  id: number
-  name: string
-  description: string
-  price: number
-  available: boolean
-  image: string
-}
+import { FoodProps } from '../../types';
 
 export const Dashboard = function () {
   const [foods, setFoods] = useState<FoodProps[]>([])
