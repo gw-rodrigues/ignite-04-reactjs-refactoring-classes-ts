e8e60d81e69357eabd632e2bb5055f95f9446274
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-04-18 11:26:55 +0200

    Refactoring tsx, interface, functions and vars

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,28 @@
+import { FiPlusSquare } from 'react-icons/fi';
+
+import { Container } from './styles';
+import Logo from '../../assets/logo.svg';
+import { HeaderProps } from '../../types';
+
+export const Header = function ({ openModal }: HeaderProps) {
+  return (
+    <Container>
+      <header>
+        <img src={Logo} alt="GoRestaurant" />
+        <nav>
+          <div>
+            <button
+              type="button"
+              onClick={openModal}
+            >
+              <div className="text">Novo Prato</div>
+              <div className="icon">
+                <FiPlusSquare size={24} />
+              </div>
+            </button>
+          </div>
+        </nav>
+      </header>
+    </Container>
+  )
+};
