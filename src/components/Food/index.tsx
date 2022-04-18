e2cb19f9c3415d76341f6d53465c86eda0d93daf
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-04-18 22:41:55 +0200

    Fixing getting wrong state after editiing

--- src/components/Food/index.tsx
+++ src/components/Food/index.tsx
@@ -8,7 +8,7 @@ import { propsAndFoodProps } from '../../types';
 
 export const Food = function ({ food, handleDelete, handleEditFood }: propsAndFoodProps) {
   const [isAvailable, setIsAvailable] = useState(food.available);
-  
+
   const toggleAvailable = async () => {
     await api.put(`/foods/${food.id}`, {
       ...food,
@@ -18,57 +18,60 @@ export const Food = function ({ food, handleDelete, handleEditFood }: propsAndFo
   }
 
   const setEditingFood = () => {
-    handleEditFood(food);
+    //Trying to fix error, when changing isAvailable on toogleAvailable
+    //and Edit the Food, save it, its get old isAvailable value 
+    //ex: isAvailable = true <-> food.available = false
+    handleEditFood({ ...food, available: isAvailable });
   }
 
-    return (
-      <Container available={isAvailable}>
-        <header>
-          <img src={food.image} alt={food.name} />
-        </header>
-        <section className="body">
-          <h2>{food.name}</h2>
-          <p>{food.description}</p>
-          <p className="price">
-            R$ <b>{food.price}</b>
-          </p>
-        </section>
-        <section className="footer">
-          <div className="icon-container">
-            <button
-              type="button"
-              className="icon"
-              onClick={setEditingFood}
-              data-testid={`edit-food-${food.id}`}
-            >
-              <FiEdit3 size={20} />
-            </button>
+  return (
+    <Container available={isAvailable}>
+      <header>
+        <img src={food.image} alt={food.name} />
+      </header>
+      <section className="body">
+        <h2>{food.name}</h2>
+        <p>{food.description}</p>
+        <p className="price">
+          R$ <b>{food.price}</b>
+        </p>
+      </section>
+      <section className="footer">
+        <div className="icon-container">
+          <button
+            type="button"
+            className="icon"
+            onClick={setEditingFood}
+            data-testid={`edit-food-${food.id}`}
+          >
+            <FiEdit3 size={20} />
+          </button>
 
-            <button
-              type="button"
-              className="icon"
-              onClick={() => handleDelete(food.id)}
-              data-testid={`remove-food-${food.id}`}
-            >
-              <FiTrash size={20} />
-            </button>
-          </div>
+          <button
+            type="button"
+            className="icon"
+            onClick={() => handleDelete(food.id)}
+            data-testid={`remove-food-${food.id}`}
+          >
+            <FiTrash size={20} />
+          </button>
+        </div>
 
-          <div className="availability-container">
-            <p>{isAvailable ? 'Disponível' : 'Indisponível'}</p>
+        <div className="availability-container">
+          <p>{isAvailable ? 'Disponível' : 'Indisponível'}</p>
 
-            <label htmlFor={`available-switch-${food.id}`} className="switch">
-              <input
-                id={`available-switch-${food.id}`}
-                type="checkbox"
-                checked={isAvailable}
-                onChange={toggleAvailable}
-                data-testid={`change-status-food-${food.id}`}
-              />
-              <span className="slider" />
-            </label>
-          </div>
-        </section>
-      </Container>
-    );
+          <label htmlFor={`available-switch-${food.id}`} className="switch">
+            <input
+              id={`available-switch-${food.id}`}
+              type="checkbox"
+              checked={isAvailable}
+              onChange={toggleAvailable}
+              data-testid={`change-status-food-${food.id}`}
+            />
+            <span className="slider" />
+          </label>
+        </div>
+      </section>
+    </Container>
+  );
 };
