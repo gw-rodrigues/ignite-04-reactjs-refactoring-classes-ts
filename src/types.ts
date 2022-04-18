0d832aeab46576ff9ffc64a142502c61b3a276d8
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-04-18 11:26:19 +0200

    Renaming to ts

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,5 @@
+import { boolean } from "yup"
+
 export interface FoodProps {
     id: number
     name: string
@@ -11,4 +13,15 @@ export interface propsAndFoodProps {
     food: FoodProps
     handleDelete: (id:number)=>Promise<void>
     handleEditFood: (food:FoodProps)=>void
+}
+
+export interface HeaderProps {
+    openModal: ()=>void
+}
+
+export interface propsAndModalProps {
+    modalOpen: boolean
+    editModalOpen: boolean
+    toggleModal: ()=>void
+    toggleEditModal: ()=>void
 }
