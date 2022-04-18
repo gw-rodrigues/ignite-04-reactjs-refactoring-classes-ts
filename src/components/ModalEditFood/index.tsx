39e2ba053c5e5785858284b88bab46f5fa92bda4
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-04-18 22:19:25 +0200

    Refactoring, add interface, functions and vars

---
+++ src/components/ModalEditFood/index.tsx
@@ -0,0 +1,38 @@
+import { createRef } from 'react';
+import { FiCheckSquare } from 'react-icons/fi';
+
+import { Form } from './styles';
+import Modal from '../Modal';
+import Input from '../Input';
+import { FoodProps, ModalEditFoodProps } from '../../types';
+
+export const ModalEditFood = function ({ isOpen, setIsOpen, editingFood, handleUpdateFood }: ModalEditFoodProps) {
+
+  const formRef = createRef<HTMLFormElement>();
+
+  const handleSubmit = async (data: FoodProps) => {
+    handleUpdateFood(data);
+    setIsOpen();
+  };
+
+  return (
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+      <Form ref={formRef} onSubmit={handleSubmit} initialData={editingFood}>
+        <h1>Editar Prato</h1>
+        <Input name="image" placeholder="Cole o link aqui" />
+
+        <Input name="name" placeholder="Ex: Moda Italiana" />
+        <Input name="price" placeholder="Ex: 19.90" />
+
+        <Input name="description" placeholder="Descrição" />
+
+        <button type="submit" data-testid="edit-food-button">
+          <div className="text">Editar Prato</div>
+          <div className="icon">
+            <FiCheckSquare size={24} />
+          </div>
+        </button>
+      </Form>
+    </Modal>
+  );
+};
